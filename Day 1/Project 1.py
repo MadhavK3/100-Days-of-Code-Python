f18a0b7e81e7eae1b17d2aef5b29cfd0cf814a58
MadhavK3 <madhavkhobare@gmail.com>
2025-07-28 01:19:31 +0530

    Day 1

---
+++ Day 1/Project 1.py
@@ -0,0 +1,5 @@
+# First Project
+print("Welcome To Brand Name Generator ")
+city = input("What is Your City Name ? \n")
+pet = input("What Is Your Pet Name ? \n")
+print("Brand Name Is " + city +" "+ pet)
--- Day 1/task.py
+++ Day 1/task.py
@@ -1,1 +1,13 @@
-print("hello world")
+# This script prints "Hello World" and then prints "Hello world!" three times, each on a new line.
+print("Hello World")
+print("Hello world!\nHello world!\nHello world!\n")
+print("Hello world!" *3)  # This will print "Hello world!" three times, each on a new line.
+print("Hello world!\n" * 3)  # This will print "Hello world!" three times, each on a new line.
+
+# inputs
+print("My name is " + input("What is Your Name ?")+" !")
+
+#variables
+a = len(input("What is your name?"))
+print(a)
+ 
